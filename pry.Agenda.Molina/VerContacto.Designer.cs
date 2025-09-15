34a4fbdff50da1b3e498a395ee477b1d49433aae
guadamolina06 <guadaaalupemolinaaa@gmail.com>
2025-09-15 14:21:14 -0300

    se cambiaron datos

---
+++ pry.Agenda.Molina/VerContacto.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace pry.Agenda.Molina
+{
+    partial class VerContacto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnAtras = new Button();
+            lblVerContacto = new Label();
+            btnSiguiente = new Button();
+            SuspendLayout();
+            // 
+            // btnAtras
+            // 
+            btnAtras.BackColor = SystemColors.ActiveBorder;
+            btnAtras.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnAtras.Location = new Point(49, 287);
+            btnAtras.Name = "btnAtras";
+            btnAtras.Size = new Size(166, 60);
+            btnAtras.TabIndex = 1;
+            btnAtras.Text = "Atras";
+            btnAtras.UseVisualStyleBackColor = false;
+            btnAtras.Click += btnAtras_Click;
+            // 
+            // lblVerContacto
+            // 
+            lblVerContacto.BorderStyle = BorderStyle.Fixed3D;
+            lblVerContacto.Font = new Font("Segoe UI Black", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblVerContacto.Location = new Point(45, 26);
+            lblVerContacto.Name = "lblVerContacto";
+            lblVerContacto.Size = new Size(367, 231);
+            lblVerContacto.TabIndex = 2;
+            lblVerContacto.Text = "_";
+            lblVerContacto.TextAlign = ContentAlignment.MiddleCenter;
+            lblVerContacto.Click += lblVerContacto_Click;
+            // 
+            // btnSiguiente
+            // 
+            btnSiguiente.BackColor = SystemColors.ActiveBorder;
+            btnSiguiente.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSiguiente.Location = new Point(246, 287);
+            btnSiguiente.Name = "btnSiguiente";
+            btnSiguiente.Size = new Size(166, 60);
+            btnSiguiente.TabIndex = 3;
+            btnSiguiente.Text = "Siguiente";
+            btnSiguiente.UseVisualStyleBackColor = false;
+            btnSiguiente.Click += btnSiguiente_Click;
+            // 
+            // VerContacto
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            BackgroundImageLayout = ImageLayout.Center;
+            ClientSize = new Size(449, 368);
+            Controls.Add(btnAtras);
+            Controls.Add(btnSiguiente);
+            Controls.Add(lblVerContacto);
+            Name = "VerContacto";
+            Text = "VerContacto";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnAtras;
+        private Label lblVerContacto;
+        private Button btnSiguiente;
+    }
+}
---
+++ pry.Agenda.Molina/VerContacto.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pry.Agenda.Molina
+{
+    public partial class VerContacto : Form
+    {
+        public VerContacto()
+        {
+            InitializeComponent();
+        }
+        //Declaracion de vector
+        int indice = 0;
+        string[] vecNombre = new string[3];
+
+        private void lblVerContacto_Click(object sender, EventArgs e)
+        {
+            //Cargar vector
+            vecNombre[0] = "Juan Perez";
+            vecNombre[1] = "Maria Gomez";
+            vecNombre[2] = "Carlos Sanchez";
+            //Mostrar primer contacto
+            lblVerContacto.Text = vecNombre[0];
+        }
+
+        private void btnSiguiente_Click(object sender, EventArgs e)
+        {
+            //sumar indice (indice = indice + 1)
+            indice++;
+
+            lblVerContacto.Text = vecNombre[indice];
+
+            
+            if (vecNombre.Length <= (indice + 1))
+            //siempre que este en el ultimo contacto
+            {
+                btnSiguiente.Enabled = false;
+            }
+            else //se habilita el boton atras
+                 //siempre que no este en el primer contacto
+            {
+                btnAtras.Enabled = true;
+            }
+        }
+
+        private void btnAtras_Click(object sender, EventArgs e)
+        {
+            //restar indice (indice = indice - 1)
+            indice--;
+            lblVerContacto.Text = vecNombre[indice];
+            //siempre que no este en el primer contacto
+            if (indice == 0)
+            {
+                btnAtras.Enabled = false;
+            }
+            else //se habilita el boton siguiente
+            {
+                btnSiguiente.Enabled = true;
+            }
+        }
+    }
+}
